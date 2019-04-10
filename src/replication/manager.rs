eea735f93149f48f67b6865fd0b9e45920dae8a5
doyoubi <doyoubihgx@gmail.com>
2019-04-10 09:36:29 +0800

    Avoid duplicated replicators for the same node

--- src/replication/manager.rs
+++ src/replication/manager.rs
@@ -2,19 +2,20 @@ use super::redis_replicator::{RedisMasterReplicator, RedisReplicaReplicator};
 use super::replicator::{MasterMeta, MasterReplicator, ReplicaMeta, ReplicaReplicator};
 use chashmap::CHashMap;
 use futures::Future;
+use itertools::Either;
 use std::sync::Arc;
 use tokio;
 
+type ReplicatorEnum = Either<Arc<MasterReplicator>, Arc<ReplicaReplicator>>;
+
 pub struct ReplicatorManager {
-    master_replicators: CHashMap<String, Arc<MasterReplicator>>,
-    replica_replicators: CHashMap<String, Arc<ReplicaReplicator>>,
+    replicators: CHashMap<String, ReplicatorEnum>,
 }
 
 impl Default for ReplicatorManager {
     fn default() -> Self {
         Self {
-            master_replicators: CHashMap::new(),
-            replica_replicators: CHashMap::new(),
+            replicators: CHashMap::new(),
         }
     }
 }
@@ -23,8 +24,8 @@ impl ReplicatorManager {
     pub fn update_masters(&self, meta_array: Vec<MasterMeta>) {
         for meta in meta_array.into_iter() {
             let master_address = meta.master_node_address.clone();
-            let replicator = match self.master_replicators.get(&master_address) {
-                Some(ref r) if r.get_meta().eq(&meta) => continue,
+            let replicator = match self.replicators.get(&master_address) {
+                Some(ref r) if r.as_ref().either(|m| m.get_meta().eq(&meta), |_| false) => continue,
                 _ => Arc::new(RedisMasterReplicator::new(meta)),
             };
             tokio::spawn(
@@ -32,15 +33,16 @@ impl ReplicatorManager {
                     .start()
                     .map_err(|e| error!("master replicator exit {:?}", e)),
             );
-            self.master_replicators.insert(master_address, replicator);
+            self.replicators
+                .insert(master_address, Either::Left(replicator));
         }
     }
 
     pub fn update_replicas(&self, meta_array: Vec<ReplicaMeta>) {
         for meta in meta_array.into_iter() {
             let replica_address = meta.replica_node_address.clone();
-            let replicator = match self.replica_replicators.get(&replica_address) {
-                Some(ref r) if r.get_meta().eq(&meta) => continue,
+            let replicator = match self.replicators.get(&replica_address) {
+                Some(ref r) if r.as_ref().either(|_| false, |r| r.get_meta().eq(&meta)) => continue,
                 _ => Arc::new(RedisReplicaReplicator::new(meta)),
             };
             tokio::spawn(
@@ -48,7 +50,8 @@ impl ReplicatorManager {
                     .start()
                     .map_err(|e| error!("replica replicator exit {:?}", e)),
             );
-            self.replica_replicators.insert(replica_address, replicator);
+            self.replicators
+                .insert(replica_address, Either::Right(replicator));
         }
     }
 }
