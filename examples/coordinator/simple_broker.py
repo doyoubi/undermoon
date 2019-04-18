c791ecdb1429deedca5d32d2368dfd52468d9216
doyoubi <doyoubihgx@gmail.com>
2019-04-18 23:11:50 +0800

    Add tests for broker script

--- examples/coordinator/simple_broker.py
+++ examples/coordinator/simple_broker.py
@@ -1,11 +1,10 @@
 import sys
 import time
-from collections import defaultdict
 from copy import deepcopy
 import logging
 
 import redis
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 logger = logging.getLogger('simple_broker')
@@ -26,6 +25,10 @@ app = Flask(__name__)
 MASTER = 'master'
 REPLICA = 'replica'
 DB_NAME = 'mydb'
+
+
+class BrokerError(Exception):
+    pass
 
 
 class ReplPeer:
@@ -75,7 +78,7 @@ class Node:
             'proxy_address': self.proxy_address,
             'cluster_name': self.cluster_name,
             'repl': self.repl.to_dict(),
-            'slots': [self.slots.to_dict()],
+            'slots': [self.slots.to_dict()] if self.slots else None,
         }
 
 
@@ -111,15 +114,16 @@ def gen_meta_config(cluster_name):
     }
 
     meta_config = {}
-    for i, (proxy_address, nodes) in TOPO_CONFIG.items():
+    for i, (proxy_address, nodes) in enumerate(TOPO_CONFIG.items()):
+        i += 1
         master_address = nodes['master']
         replica_address = nodes['replica']
 
-        replica_proxy = 'proxy{}'.format(i % 3 + 1)
+        replica_proxy = 'server_proxy{i}:600{i}'.format(i=i % 3 + 1)
         replica_node = TOPO_CONFIG[replica_proxy]['replica']
         master_repl = ReplMeta(MASTER, [ReplPeer(replica_node, replica_proxy)])
 
-        master_proxy = 'proxy{}'.format((i + 1) % 3 + 1)
+        master_proxy = 'server_proxy{i}:600{i}'.format(i=(i + 1) % 3 + 1)
         master_node = TOPO_CONFIG[master_proxy]['master']
         replica_repl = ReplMeta(REPLICA, [ReplPeer(master_node, master_proxy)])
 
@@ -127,7 +131,7 @@ def gen_meta_config(cluster_name):
                                  INIT_SLOTS_CONFIG[redis1][1])
         master = Node(master_address, proxy_address, cluster_name,
                       master_slots, master_repl)
-        replica = Node(replica_address, proxy_address, cluster_name, [], replica_repl)
+        replica = Node(replica_address, proxy_address, cluster_name, None, replica_repl)
 
         meta_config[proxy_address] = {
             'master': master,
@@ -145,6 +149,7 @@ class Failure:
 
 
 def check_alive(address):
+    print(address)
     host, port = address.split(':')
     try:
         redis.StrictRedis(host, port, socket_timeout=0.1).ping()
@@ -160,14 +165,15 @@ class MetaStore:
         self.cluster_name = cluster_name
         self.proxies = gen_meta_config(cluster_name)
 
-        self.failed_proxies = defaultdict(dict)
+        self.failed_proxies = {}
         self.replaced_master = set()
 
     def get_failed_proxies(self):
         new_failed_proxies = {}
-        for address, proxy in self.failed_proxies:
+        for address, failure in self.failed_proxies.items():
+            # For simplicity, just check it to remove stale failure.
             if not check_alive(address):
-                new_failed_proxies[address] = proxy
+                new_failed_proxies[address] = failure
         self.failed_proxies = new_failed_proxies
 
         if not new_failed_proxies and self.replaced_master:
@@ -179,12 +185,12 @@ class MetaStore:
         failed_master = self.proxies[failed_proxy][MASTER]
         replica = failed_master.repl.peers[0]
         replica_proxy = replica.proxy_address
-        replica_node = replica.node_address
 
         replica = deepcopy(self.proxies[replica_proxy]['replica'])
         replica.role = MASTER
         replica.slots = failed_master.slots
         replica.repl = ReplMeta(MASTER, [])
+        return replica
 
     def get_proxies(self):
         failed = self.get_failed_proxies().keys()
@@ -192,11 +198,11 @@ class MetaStore:
             return self.proxies
 
         if len(failed) > 1:
-            raise Exception('cluster is down')
+            raise BrokerError('cluster is down')
 
-        replica = self.get_replaced_node(failed[0])
-        proxies = {a: deepcopy(p) for a, p in self.proxies if a not in failed}
-        proxies[replica.proxy_address] = replica
+        new_master = self.get_replaced_node(failed[0])
+        proxies = {a: deepcopy(p) for a, p in self.proxies.items() if a not in failed}
+        proxies[new_master.proxy_address][REPLICA] = new_master
         return proxies
 
     def get_nodes(self):
@@ -241,7 +247,7 @@ class MetaStore:
         }
 
     def report_failure(self, proxy_address, reporter_id):
-        self.failed_proxies[proxy_address][reporter_id] = Failure(proxy_address, reporter_id)
+        self.failed_proxies[proxy_address] = Failure(proxy_address, reporter_id)
 
     def get_failures(self):
         return {
@@ -250,6 +256,7 @@ class MetaStore:
 
     def replace_node(self, failed_node):
         '''
+        failed_node:
         {
             "cluster_epoch": 1,
             "node": {
@@ -272,18 +279,55 @@ class MetaStore:
         }
         '''
         if failed_node['cluster_epoch'] < self.epoch:
-            logger.warning('lower epoch {} < {}', failed_node['cluster_epoch'], self.epoch)
-            return None
+            err_msg = 'lower epoch {} < {}'.format(failed_node['cluster_epoch'], self.epoch)
+            logger.warning(err_msg)
+            raise BrokerError(err_msg)
 
-        self.replaced_master.add(failed_node['node']['address'])
+        node_address = failed_node['node']['address']
+        proxy_address = failed_node['node']['proxy_address']
+
+        if self.proxies[proxy_address][MASTER].address != node_address:
+            raise BrokerError('Can not create new replica')
+
+        self.replaced_master.add(node_address)
         self.epoch += 1
-        return self.get_replaced_node(failed_node['node']['proxy_address']).to_dict()
+        return self.get_replaced_node(proxy_address).to_dict()
 
 
 meta_store = MetaStore(DB_NAME)
 
 
-@app.route("/api/clusters/names")
-def hello():
+@app.route('/api/clusters/names')
+def get_cluster_names():
     return jsonify(meta_store.get_cluster_names())
 
+
+@app.route('/api/clusters/name/<cluster_name>')
+def get_cluster(cluster_name):
+    return jsonify(meta_store.get_cluster(cluster_name))
+
+
+@app.route('/api/hosts/addresses')
+def get_proxies():
+    return jsonify(meta_store.get_proxy_addresses())
+
+
+@app.route('/api/hosts/address/<server_proxy_address>')
+def get_proxy(server_proxy_address):
+    return jsonify(meta_store.get_proxy(server_proxy_address))
+
+
+@app.route('/api/failures/<server_proxy_address>/<reporter_id>', methods=['POST'])
+def report_failure(server_proxy_address, reporter_id):
+    return jsonify(meta_store.report_failure(server_proxy_address, reporter_id))
+
+
+@app.route('/api/failures')
+def get_failures():
+    return jsonify(meta_store.get_failures())
+
+
+@app.route('/api/clusters/nodes', methods=['PUT'])
+def replace_node():
+    failed_node = request.get_json()
+    return jsonify(meta_store.replace_node(failed_node))
---
+++ examples/coordinator/test_api.py
@@ -0,0 +1,67 @@
+import pprint
+
+import requests
+
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+def test_get_cluster_names():
+    names = requests.get('http://localhost:5000/api/clusters/names').json()
+    pp.pprint(names)
+
+def test_get_cluster():
+    res = requests.get('http://localhost:5000/api/clusters/name/{}'.format('mydb'))
+    cluster = res.json()
+    pp.pprint(cluster)
+
+def test_get_proxies():
+    proxies = requests.get('http://localhost:5000/api/hosts/addresses').json()
+    pp.pprint(proxies)
+
+def test_get_proxy(proxy_address):
+    proxy = requests.get('http://localhost:5000/api/hosts/address/{}'.format(proxy_address)).json()
+    pp.pprint(proxy)
+
+def test_report_failure():
+    res = requests.post('http://localhost:5000/api/failures/{}/{}'.format('server_proxy1:6001', 'doyoubi'))
+    pp.pprint(res.status_code)
+
+def test_get_failures():
+    failures = requests.get('http://localhost:5000/api/failures').json()
+    pp.pprint(failures)
+
+
+def test_replace_node():
+    payload = {
+        "cluster_epoch": 1,
+        "node": {
+            "address": "redis1:7001",
+            "proxy_address": "server_proxy1:6001",
+            "cluster_name": "mydb",
+            "repl": {
+                "role": "master",
+                "peers": [{
+                    "node_address": "redis5:7005",
+                    "proxy_address": "server_proxy2:6002",
+                }]
+            },
+            "slots": [{
+                "start": 0,
+                "end": 5461,
+                "tag": ""
+            }]
+        }
+    }
+    new_node = requests.put('http://localhost:5000/api/clusters/nodes', json=payload).json()
+    pp.pprint(new_node)
+
+test_get_cluster_names()
+test_get_cluster()
+test_get_proxies()
+test_get_proxy('server_proxy1:6001')
+test_get_proxy('server_proxy2:6002')
+test_get_proxy('server_proxy3:6003')
+test_report_failure()
+test_get_failures()
+test_replace_node()
