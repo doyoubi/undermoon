7865a92ad82d9a6064ecbae227758932f18093f7
doyoubi <doyoubihgx@gmail.com>
2020-05-04 19:22:42 +0800

    Add config api for memory broker

--- src/broker/mod.rs
+++ src/broker/mod.rs
@@ -10,5 +10,7 @@ mod update;
 
 pub use self::persistence::{JsonFileStorage, MetaStorage, MetaSyncError};
 pub use self::replication::{JsonMetaReplicator, MetaReplicator};
-pub use self::service::{configure_app, MemBrokerConfig, MemBrokerService, MEM_BROKER_API_VERSION};
+pub use self::service::{
+    configure_app, MemBrokerConfig, MemBrokerService, ReplicaAddresses, MEM_BROKER_API_VERSION,
+};
 pub use self::store::MetaStoreError;
