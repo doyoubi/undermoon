a363abd8f0c5ceddeb87c14bdcc9aaa2d260ef3e
doyoubi <doyoubihgx@gmail.com>
2020-09-13 18:57:30 +0800

    Bump version to 0.3.2

--- src/common/version.rs
+++ src/common/version.rs
@@ -1,3 +1,3 @@
-pub const UNDERMOON_VERSION: &str = "0.3.1";
+pub const UNDERMOON_VERSION: &str = "0.3.2";
 pub const UNDERMOON_MIGRATION_VERSION: &str = "mgr-0.2";
 pub const UNDERMOON_MEM_BROKER_META_VERSION: &str = "mem-broker-0.2";
