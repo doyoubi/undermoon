6d4facc4ebf3346fafa638d93fd7f6f5907c20ff
doyoubi <doyoubihgx@gmail.com>
2020-10-22 21:32:41 +0800

    Bump version to 0.4.0-alpha.2

--- src/common/version.rs
+++ src/common/version.rs
@@ -1,3 +1,3 @@
-pub const UNDERMOON_VERSION: &str = "0.4.0-alpha.1";
+pub const UNDERMOON_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const UNDERMOON_MIGRATION_VERSION: &str = "mgr-0.2";
 pub const UNDERMOON_MEM_BROKER_META_VERSION: &str = "mem-broker-0.2";
